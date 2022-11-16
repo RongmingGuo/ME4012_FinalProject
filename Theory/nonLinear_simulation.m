249e14cc9856a9783156d095907d1c04615a1ec3
Rongming (Tony) Guo <rguo61@gatech.edu>
2022-11-16 18:41:48 -0500

    documented theory. Updated matlab simulation, tested a PD controller

--- Theory/nonLinear_simulation.m
+++ Theory/nonLinear_simulation.m
@@ -14,7 +14,7 @@ Icc_p = cart.Icc_p;
 %% Environment Constants
 g = 9.81; % m/s^2, gravitational acceleration
 %% Initial conditions
-X = [0; 0; 0; 0];
+X = [0; 0.5; 0; 0];
 % [phi;
 % theta;
 % phi_dot;
@@ -31,16 +31,16 @@ Result = [];
 for t = 0 : tStep : tEnd
    t
    % Apply feed forward torque
-   tw = sin(10 * t) * 0.03; % oscillating 0.5 nm
+   tw = 0.3 * (X(2)) + 0.1 * (X(4)); % PD
    % Solving Differential Equations
    A = zeros(2);
    b = zeros(2, 1);
    A(1, 1) = Iw + (M*R^2)/4 + (R^2*mp)/4;
-   A(1, 2) = (L*R*mp*cos(X(2)))/4;
+   A(1, 2) = (L*R*mp*cos(X(2)))/4/4;
    b(1) = (L*R*mp*sin(X(2))*X(4)^2)/4 + tw; % EOM1
-   A(2, 1) = -L*R*mp*cos(X(2));
-   A(2, 2) = Icc_p - L^2*mp*cos(X(2))^2 - L^2*mp*sin(X(2))^2;
-   b(2) = 0;
+   A(2, 1) = L*R*mp*cos(X(2));
+   A(2, 2) = mp*L^2*cos(X(2))^2 + mp*L^2*sin(X(2))^2 + Icc_p;
+   b(2) = L*sin(X(2))*(- L*mp*cos(X(2))*X(4)^2 + g*mp) + L^2*X(4)^2*mp*cos(X(2))*sin(X(2));
    secondOrder = A\b;
    dX = [X(3); X(4); secondOrder];
    % Do the update
--- Theory/symbolicSolution.m
+++ Theory/symbolicSolution.m
@@ -7,6 +7,6 @@ syms phi dphi ddphi o do ddo
 % eqn2 = Ip * ddo == (L * cos(o)) * 4 * (tw - Iw * ddphi - M * R^2 / 4 * ddphi) / R + mp * g * L * sin(o) - L * sin(o) * mp * (-L * cos(o) * (do)^2 - L * sin(o) * ddo) - mp * g;
 
 eqn1 = (1/4 * M * R^2 + Iw) * ddphi == tw - R/4 * (mp * (R * ddphi + L * (-sin(o) * do^2 + cos(o) * ddo)));
-eqn2 = Icc_p * ddo == (mp * (R * ddphi + L * (-sin(o) * (do)^2 + cos(o) * ddo))) * L * cos(o) - mp * (-L * (cos(o) * do^2 + sin(o) * ddo)) * L * sin(o);
+eqn2 = Icc_p * ddo == (mp * (-L * (cos(o) * do^2 + sin(o) * ddo)) + mp *g) * (L * sin(o)) - mp * (R * ddphi + L * (-sin(o) * (do)^2 + cos(o) * ddo)) * L * cos(o);
 %% Equations to Matrix
 [A, b] = equationsToMatrix([eqn1, eqn2], [ddphi, ddo]);
