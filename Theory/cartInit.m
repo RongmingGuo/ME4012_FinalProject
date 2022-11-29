1c1058787f96cca4ab75cec5f9605b138d6a7d27
Rongming (Tony) Guo <rguo61@gatech.edu>
2022-11-29 12:59:21 -0500

    updated cart parameters. Still need cart mass to be measured

--- Theory/cartInit.m
+++ Theory/cartInit.m
@@ -1,15 +1,15 @@
 % This file stores all the parameters of the cart in a struct
 cart = struct();
-%% Geometry
+%% Geometry (Primarily for Plotting in Simulation)
 cart.a = 0.1; % m, half-chassis length
 cart.b = 0.005; % m, extension of wheel from chassis
 cart.R = 0.03; % m, radius of the wheel
 cart.H = 0.054; % m, chassis ground clerance
 cart.h = 0.04; % m, pole height
 cart.L = 0.15; % m, half pendulum arm length
-%% Dynamics
-cart.mw = 0.1; % kg, mass of the wheel
-cart.Iw = 2/3 * cart.mw * cart.R^2; % kg*m^2, rotational inertia of the wheel
+%% Mass & Inertia
+cart.mw = 0.02839; % kg, mass of the wheel, obtained from Solidworks
+cart.Iw = 13966.17 * 10^-9; % kg*m^2, rotational inertia of the wheel, obtained from Solidworks
 cart.M = 0.5; % kg, entire cart mass, excluding the pendulum
-cart.mp = 0.2; % kg, mass of the pendulum
-cart.Icc_p = 1/3 * cart.mp * (cart.L)^2; % kg*m^2, rotational inertia of the physical pendulum with respect to its center of mass
+cart.mp = 0.18378; % kg, mass of the pendulum
+cart.Icc_p = 1/12 * cart.mp * (2 * cart.L)^2; % kg*m^2, rotational inertia of the physical pendulum with respect to its center of mass
